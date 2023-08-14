02f63e36ad0aaa1a5d2b3726c69312dbddf5f274
Freakoverse <136988523+Freakoverse@users.noreply.github.com>
2023-08-14 19:19:18 +0300

    Add files via upload

---
+++ nostrplebs/assets/js/navDropdown.js
@@ -0,0 +1,8 @@
+function burgerNavMain() {
+  var x = document.getElementById("navlinksID");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
